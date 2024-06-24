a239f5e9625fcc4b7ea16c531db8d13b8124b1bc
hashthat <153950283+hashthat@users.noreply.github.com>
2024-06-23 18:32:34 -0600

    the magik has arrived!

---
+++ Magik8Ball/Magik8Ball/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Magik8Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(451, 448);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ASK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Ravie", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.textBox1.Location = new System.Drawing.Point(181, 448);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 39);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Magik8Ball.Properties.Resources._00_magic_8_ball_vector_pack;
+            this.pictureBox1.Location = new System.Drawing.Point(181, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(343, 334);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(176, 415);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(406, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "WRITE YOUR QUANDARY BELOW!!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 714);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Magik8Ball/Magik8Ball/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// https://www.vecteezy.com/vector-art/87501-magic-8-ball-vector-pack
+namespace Magik8Ball
+{
+    // Naturally created with the form from creating the button and the App in WinForm
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Button object was created and the object was added to the main script.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 15); // Generates a random number between 1 and 15
+
+            switch (randomNumber)
+            {
+                // the case statement sorts through the local files which display the image of the 8ball for the answer to the question
+                case 1:
+                    pictureBox1.Image = Properties.Resources._00_magic_8_ball_vector_pack;
+                    break; // break is needed to break the sift to go on to the next picture in the file.
+                case 2:
+                    pictureBox1.Image = Properties.Resources._01_magic_8_ball_vector_pack;
+                    break; // the pictureBox1 is the properties tab to the right as the code is locating the image
+                case 3:
+                    pictureBox1.Image = Properties.Resources._02_magic_8_ball_vector_pack;
+                    break; // the Image is found in the Resources, and then each picturebox1 element is expressed as the photo is used and found by the random number generator.
+                case 4:
+                    pictureBox1.Image = Properties.Resources._03_magic_8_ball_vector_pack;
+                    break; // All the cases are the same in form, the images and the image numbers are different. but it works!
+                case 5:
+                    pictureBox1.Image = Properties.Resources._04_magic_8_ball_vector_pack;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources._05_magic_8_ball_vector_pack;
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources._06_magic_8_ball_vector_pack;
+                    break;
+                case 8:
+                    pictureBox1.Image = Properties.Resources._07_magic_8_ball_vector_pack;
+                    break;
+                case 9:
+                    pictureBox1.Image = Properties.Resources._08_magic_8_ball_vector_pack;
+                    break;
+                case 10:
+                    pictureBox1.Image = Properties.Resources._09_magic_8_ball_vector_pack;
+                    break;
+                case 11:
+                    pictureBox1.Image = Properties.Resources._10_magic_8_ball_vector_pack;
+                    break;
+                case 12:
+                    pictureBox1.Image = Properties.Resources._11_magic_8_ball_vector_pack;
+                    break;
+                case 13:
+                    pictureBox1.Image = Properties.Resources._12_magic_8_ball_vector_pack;
+                    break;
+                case 14:
+                    pictureBox1.Image = Properties.Resources._13_magic_8_ball_vector_pack;
+                    break;
+                case 15:
+                    pictureBox1.Image = Properties.Resources._14_magic_8_ball_vector_pack;
+                    break;
+
+
+            }
+        }
+    }
+}
